1a4eb92da21094190a5207e095fd1c9321acb1ab
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2022-04-06 10:14:44 +0100

    students added

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,11 +1,23 @@
 package com;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+import com.models.Students;
 
 public class Main {
 	public static void main(String[] args) {
 		Session session = HibernateUtil.getSessionFactory().openSession();
-		session.beginTransaction();
+		Transaction transaction = session.beginTransaction();
+		
+		Students ahmad = new Students();
+		ahmad.setFullName("Ahmad Muhammad");
+		ahmad.setAge(20);
+		ahmad.setGender("male");
+		
+		session.save(ahmad);
+		transaction.commit();
+		
 		session.close();
 	}
 
