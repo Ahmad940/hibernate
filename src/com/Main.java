4d8cbd15a29179361f92e8bcd07424e44d43a720
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2022-04-06 09:55:11 +0100

    students added

--- src/com/Main.java
+++ src/com/Main.java
@@ -4,7 +4,6 @@ import org.hibernate.Session;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
 		session.close();
