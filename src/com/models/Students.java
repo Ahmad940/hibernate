b460207113de7e53b1cb51a113c6a054482a1301
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2022-04-06 09:33:15 +0100

    model

---
+++ src/com/models/Students.java
@@ -0,0 +1,40 @@
+package com.models;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity()
+public class Students {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+	private String fullName, gender;
+	private int age;
+	
+	
+	public String getFullName() {
+		return fullName;
+	}
+	public void setFullName(String fullName) {
+		this.fullName = fullName;
+	}
+	public String getGender() {
+		return gender;
+	}
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public int getId() {
+		return id;
+	}
+	
+	
+}
