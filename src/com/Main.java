4ac61657a965409b55bb53ee1cc745987028b7d3
Ahmad940 <ahmadmuhammadmak5@gmail.com>
2022-04-11 10:37:14 +0100

    updated sms

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,5 +1,7 @@
 package com;
 
+import java.util.List;
+
 import org.hibernate.Session;
 import org.hibernate.Transaction;
 
@@ -11,12 +13,28 @@ public class Main {
 		Transaction transaction = session.beginTransaction();
 		
 		Students ahmad = new Students();
-		ahmad.setFullName("Ahmad Muhammad");
-		ahmad.setAge(20);
+		ahmad.setId(2);
+		ahmad.setFullName("Ahmad Kabir");
+		ahmad.setAge(15);
 		ahmad.setGender("male");
 		
-		session.save(ahmad);
+//		session.save(ahmad);
+//		session.update(ahmad);
+		
+		List<Students> studentsLists = 
+				session.createQuery
+				("SELECT a FROM Students a", Students.class)
+				.getResultList();
+		studentsLists.forEach(student -> {
+			System.out.println("ID: " + student.getId());
+			System.out.println("Fullname: " + student.getFullName());
+			System.out.println("Age: " + student.getAge());
+			System.out.println("Gender: " + student.getGender());
+		});
+		
+		
 		transaction.commit();
+//		
 		
 		session.close();
 	}
--- src/com/models/Students.java
+++ src/com/models/Students.java
@@ -35,6 +35,11 @@ public class Students {
 	public int getId() {
 		return id;
 	}
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
 	
 	
 }
